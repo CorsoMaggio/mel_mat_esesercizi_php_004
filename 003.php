fef28652b78fa8c8f91a76955e49691f7b138780
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-12 16:39:03 +0200

    commit con domanda

---
+++ 003.php
@@ -0,0 +1,47 @@
+<?php
+
+/*Creare una struttura a Classi sfruttando l’ereditarietà e seguendo queste semplici regole:
+
+1)Le classi NON devono avere attributi;
+
+2)Ogni classe avrà un metodo specifico PROTECTED per stampare la sua specializzazione (Attenti all’overwrite);
+
+Non Potete realizzare metodi definiti dall’utente PUBLIC per stampare il risultato, l’unico metodo PUBLIC ammesso è il Costruttore;
+Esempio di Output:
+
+$magikarp = new Fish();
+
+//Nel terminale visaulizzerete:
+
+Sono un animale Vertebrato
+Sono un animale a Sangue Freddo
+Splash!
+
+//---------------------
+
+$serpente = new Reptile();
+
+//Nel terminale visaulizzerete:
+
+Sono un animale Vertebrato
+Sono un animale a Sangue Freddo
+Sono un Serepnte
+*/
+abstract class Vertebrate
+{
+    public function __construct()
+    {
+        self::imVertebrate();
+    }
+    //DOMANDA PER FRANCESCO: SELF è IL MODO GIUSTO PER RICHIAMARLO CON SE STESSO??
+
+    protected function imVertebrate()
+    {
+        echo "Sono un animale vertebrato";
+    }
+}
+
+class WarmBlooded extends Vertebrate
+{
+    public function __construct() {}
+}
