871dd954fb006eb65859483a037c2ecf51477c58
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-12 17:28:17 +0200

    fine esercizio2

--- 003.php
+++ 003.php
@@ -31,13 +31,13 @@ abstract class Vertebrate
 {
     public function __construct()
     {
-        self::imVertebrate();
+        $this->imVertebrate();
     }
     //DOMANDA PER FRANCESCO: SELF è IL MODO GIUSTO PER RICHIAMARLO CON SE STESSO??
 
     protected function imVertebrate()
     {
-        echo "Sono un animale vertebrato";
+        echo "Sono un animale vertebrato.\n";
     }
 }
 
@@ -45,11 +45,92 @@ class WarmBlooded extends Vertebrate
 {
     public function __construct()
     {
-        parent::imVertebrate();
-        self::imWarmBlooded();
+        parent::__construct();
+        $this->imWarmBlooded();
     }
     protected function imWarmBlooded()
     {
-        echo "Sono un animale a sangue caldo";
+        echo "Sono un animale a sangue caldo.\n";
     }
 }
+class ColdBlooded extends Vertebrate
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imColdBlooded();
+    }
+    protected function imColdBlooded()
+    {
+        echo "Sono un animale a sangue freddo.\n";
+    }
+}
+class Fish extends ColdBlooded
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imFish();
+    }
+    protected function imFish()
+    {
+        echo "Sono un pesce.\n Spero di evolvermi presto in un Gyarados.\n";
+    }
+}
+class Reptile extends ColdBlooded
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imReptile();
+    }
+    protected function imReptile()
+    {
+        echo "Sono un rettile e parlo serpentese.\n LE TARTARUGHE NON SONO ANFIBI!!\n";
+    }
+}
+
+class Amphibian extends ColdBlooded
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imAmphibian();
+    }
+    protected function imAmphibian()
+    {
+        echo "Sono un anfibio.\n NON SONO UNA TARTARUGA.\n";
+    }
+}
+class Mammal extends WarmBlooded
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imMammal();
+    }
+    protected function imMammal()
+    {
+        echo "Roarrrrr.\n";
+    }
+}
+
+class Bird extends WarmBlooded
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imBird();
+    }
+    protected function imBird()
+    {
+        echo "Chip chip.\n";
+    }
+}
+
+
+$lion = new Mammal;
+$hummingbird = new Bird;
+$magikarp = new Fish;
+$turtle = new Reptile;
+$frog = new Amphibian;
